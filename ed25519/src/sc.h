e9d7589fd83b902e78fa4ce73e0a7e3b642e9d91
IGN <mcshe@DESKTOP-SQT517A>
2018-07-23 14:30:18 +0300

    Commit 7. Add ed25519 encryption module

---
+++ ed25519/src/sc.h
@@ -0,0 +1,8 @@
+#ifndef SC_H
+#define SC_H
+
+
+void sc_reduce(unsigned char *s);
+void sc_muladd(unsigned char *s, const unsigned char *a, const unsigned char *b, const unsigned char *c);
+
+#endif
